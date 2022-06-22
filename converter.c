651e9eab98838004f182afbbb3238f167bae602b
ge32ber <ge32ber@mytum.de>
2022-06-22 15:24:12 +0200

    init

---
+++ converter.c
@@ -0,0 +1,14 @@
+#include "stdlib.h"
+
+// passes complex numbers in pointers and changes the values in them
+void coplexToPower(__int128* real, __int128* imag) {
+    //TODO: implement
+}
+
+void convertNumberToBasis(unsigned __int128 from, __int128* real, __int128* imag) {
+    // TODO: implement
+}
+
+unsigned __int128 to_bm1pi(__int128 real, __int128 imag) {
+    // TODO; implement
+}
