3e2062823a267717205398cc87e6b03ab3e5ab01
ge32ber <ge32ber@mytum.de>
2022-06-22 20:54:40 +0200

    added comments with sources and explanation

--- converter.c
+++ converter.c
@@ -2,7 +2,10 @@
 
 // computes a = (-1 + i) ^ power
 // returns R(a) und Im(a) in pointers
+// makes use of the De Moivre's formula (a + bi)^x = \sqrt{a^2 + b^2}^x (\cos{\theta} + \sin{\theta})
+// source/explanation: https://en.wikipedia.org/wiki/De_Moivre%27s_formula
 void complexToPower(int power, __int128* real, __int128* imag) {
+    // values for trigonometric functions for \theta = {0, 3/4 \pi, 1/2 \pi, 1/4 \pi}
     int cos[] = {1, 0, -1, 0};
     int sin[] = {0, -1, 0, 1};
 
@@ -13,12 +16,14 @@ void complexToPower(int power, __int128* real, __int128* imag) {
     }
 
     __int128 length = 1;
+
+    // constant used
     length =  length << (tempPow >> 1);
 
     *real = length * cos[(tempPow & 7) >> 1];
     *imag = length * sin[(tempPow & 7) >> 1];
 
-    // compute (-1 + i) ^ tpower when power is not even
+    // compute (-1 + i) ^tpower when power is not even
     if (power & 1) {
         // might be slower than when using temporary variables to store the results
         *imag = *real - *imag;
@@ -26,6 +31,7 @@ void complexToPower(int power, __int128* real, __int128* imag) {
     }
 }
 
+// conversion done through representing the number as a sum of powers of (-1 + i)
 void convertNumberToBasis(unsigned __int128 from, __int128* real, __int128* imag) {
     *real = 0;
     *imag = 0;
@@ -48,6 +54,7 @@ void convertNumberToBasis(unsigned __int128 from, __int128* real, __int128* imag
     free(currImag);
 }
 
+// algorithm taken from https://math.stackexchange.com/questions/1209190/base-conversion-how-to-convert-between-decimal-and-a-complex-base
 unsigned __int128 to_bm1pi(__int128 real, __int128 imag) {
     unsigned __int128 result = 0;
     unsigned __int128 digit = 1;
